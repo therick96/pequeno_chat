faca9ce5fc1ee2d4cc97a2a780cf7ac7b1828010
therick96 <erick25gomez@gmail.com>
2017-12-12 16:39:13 -0400

    Busqueda de usuario por metodo post

--- app/controllers/registro.js
+++ app/controllers/registro.js
@@ -1,9 +1,13 @@
+var usuario = require('../models/users');
 
 var registroController = function (server) {
     server.route('/registro')
         .get(function (req, res){
             res.render('./registro.html', { titulo : 'Registrarse',
                                             subtitulo : 'Registrate'});
+        })
+        .post(function (req, res) {
+            usuario.findOne({ usuario : req.body.user)
         });
 };
 
