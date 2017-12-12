c8a0b76daa5ccd7c5fe431957e50f9cabe0b5ffd
therick96 <erick25gomez@gmail.com>
2017-12-12 13:24:27 -0400

    creada pagina login

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@
 var homeC = function (server) {
     server.route('/')
         .get(function (req, res){
-            res.render('home/index');
+            res.render('home/index', {titulo : 'Entrar'});
         });
 };
 
