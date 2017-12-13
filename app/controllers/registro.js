2303cefb47841c392d1bbbf091bd59120efb3106
therick96 <erick25gomez@gmail.com>
2017-12-12 20:19:50 -0400

    mensaje al registrarse

--- app/controllers/registro.js
+++ app/controllers/registro.js
@@ -7,7 +7,23 @@ var registroController = function (server) {
                                             subtitulo : 'Registrate'});
         })
         .post(function (req, res) {
-            usuario.findOne({ usuario : req.body.user)
+            usuario.findOne({ usuario : req.body.user},
+                function (error, usr) {
+                    console.log(error, ', ', usr);
+                    if (usr){
+                        res.render('./registro.html', { titulo : 'Registrarse',
+                                                        subtitulo : 'Registrate',
+                                                        mensajito : 'error',
+                                                        color : 'red'});
+                        console.log("si");
+                    }else{
+                        res.render('./registro.html', { titulo : 'Registrarse',
+                                                        subtitulo : 'Registrate',
+                                                        mensajito : 'valido',
+                                                        color : '#58D68D'});
+                        console.log("NO");
+                    }
+                });
         });
 };
 
